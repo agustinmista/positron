b85697501234afa51673a02cefafdfe2e924950e
Agustín Mista <agustin@mista.me>
2023-02-11 14:27:01 +0000

    Fix devcontainer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -6,6 +6,9 @@ set -e
 sudo apt update
 sudo apt install -y libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libgtk3.0-cil libgbm1 libasound2 wine64
 
+# Install dev environment
+npm install
+
 # Fix permissions of chrome-sandbox
-sudo chown -f root:root /workspaces/positron/node_modules/electron/dist/chrome-sandbox
-sudo chmod -f 4755 /workspaces/positron/node_modules/electron/dist/chrome-sandbox
+# sudo chown -f root:root /workspaces/positron/node_modules/electron/dist/chrome-sandbox
+# sudo chmod -f 4755 /workspaces/positron/node_modules/electron/dist/chrome-sandbox
