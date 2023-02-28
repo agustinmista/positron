299138602f1fe49bc6caa1a27982913645cdfb32
Agustín Mista <agustin@mista.me>
2023-02-28 21:43:56 +0100

    Add some documentation about Alpine's limitation

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -7,5 +7,10 @@ import './css/style.css';
 // Alpine intitialization
 // ----------------------------------------
 
+// We need to use Object.assign because Alpine doesn't quite support dealing
+// with class objects. This way we transform a `new App` into a regular object
+// that Alpine knows how to instrument. Moreover, for this to work, the class
+// methods must be deconstructed and defined using the `function` keyword.
+
 alpine.data('app', () => { return Object.assign(new App()) });
 alpine.start();
