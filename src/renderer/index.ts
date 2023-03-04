aa0c51ab5565084ec7edbc4214a8850968611542
Agustín Mista <agustin@mista.me>
2023-03-04 15:22:08 +0100

    Remove old comment

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -9,13 +9,7 @@ import './css/style.css';
 // Alpine intitialization
 // ----------------------------------------
 
-// We need to use Object.assign because Alpine doesn't quite support dealing
-// with class objects. This way we transform a `new App` into a regular object
-// that Alpine knows how to instrument. Moreover, for this to work, the class
-// methods must be deconstructed and defined using the `function` keyword.
-
 alpine.data('app', () => { return new App() });
-
 alpine.store('settingsModal', new Modal());
 alpine.store('aboutModal',    new Modal());
 alpine.store('responseModal', new Modal());
