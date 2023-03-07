63308f4305801989617d521807e43bb0341b0b08
Agustín Mista <agustin@mista.me>
2023-03-07 23:57:13 +0100

    Format document. Improve trigger and delete buttons.

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -2,6 +2,8 @@ import alpine from 'alpinejs';
 
 import App from './components/App';
 import Modal from './components/Modal';
+import BusyButton from './components/BusyButton';
+import TwoStepButton from './components/TwoStepButton';
 
 import './css/style.css';
 
@@ -9,7 +11,10 @@ import './css/style.css';
 // Alpine intitialization
 // ----------------------------------------
 
-alpine.data('app', () => { return new App() });
+alpine.data('app',           () => { return new App() });
+alpine.data('busyButton',    () => { return new BusyButton() });
+alpine.data('twoStepButton', () => { return new TwoStepButton() });
+
 alpine.store('settingsModal', new Modal());
 alpine.store('aboutModal',    new Modal());
 alpine.store('responseModal', new Modal());
