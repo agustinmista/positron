bbd92436f3ed7ba22dcee12f26b3a6b6c27129b9
Agustín Mista <agustin@mista.me>
2023-03-07 23:56:41 +0100

    Make Modal component promise-based

--- src/renderer/components/Modal.ts
+++ src/renderer/components/Modal.ts
@@ -12,20 +12,26 @@ export default class Modal {
   // Optional data to pass to the open method
   data: string
 
+  // A promise that holds until the user presses close
+  resolve: () => void
+
   constructor() {
     this.isOpen = false;
     this.data = null;
+    this.resolve = null;
   }
 
   // ----------------------------------------
   // Open/close
 
-  open = function (data: string = null) {
+  open = function (data: string = null): Promise<void> {
     this.isOpen = true;
     this.data = data;
+    return new Promise(resolve => this.resolve = resolve);
   }
 
-  close = function () {
+  close = async function (): Promise<void> {
     this.isOpen = false;
+    await this.resolve(null);
   }
 }
